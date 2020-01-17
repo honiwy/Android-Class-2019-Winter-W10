5b2a7ade971036e0202679006b359798da914ad3
WYC <honiwy@gmail.com>
2020-01-17 04:27:35 -0500

    User can now choose whether he/she is Wayne or not lol

--- Midterm/app/src/main/java/org/rachel/midterm/publish/PublishDialog.kt
+++ Midterm/app/src/main/java/org/rachel/midterm/publish/PublishDialog.kt
@@ -1,19 +1,12 @@
 package org.rachel.midterm.publish
 
-import android.graphics.Rect
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.rachel.midterm.MidtermApplication
 import org.rachel.midterm.R
 import org.rachel.midterm.databinding.PublishDialogBinding
@@ -45,26 +38,4 @@ class PublishDialogFragment : DialogFragment() {
         super.onCreate(savedInstanceState)
         setStyle(STYLE_NO_FRAME, R.style.MessageDialog)
     }
-//    override fun onStart() {
-//        super.onStart()
-//
-//        val dialog = getDialog() as BottomSheetDialog
-//        val bottomSheet =
-//            dialog.delegate.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
-//        bottomSheet?.let { view ->
-//            view.layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
-//        }
-//        view?.let {
-//            it.post {
-//                kotlin.run {
-//                    val parent = it.parent as View
-//                    val params = parent.layoutParams as CoordinatorLayout.LayoutParams
-//                    val behavior = params.behavior
-//                    val bottomSheetBehavior = behavior as BottomSheetBehavior
-//                    bottomSheetBehavior.peekHeight = it.measuredHeight
-//                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-//                }
-//            }
-//        }
-//    }
 }
--- Midterm/app/src/main/java/org/rachel/midterm/publish/PublishViewModel.kt
+++ Midterm/app/src/main/java/org/rachel/midterm/publish/PublishViewModel.kt
@@ -1,13 +1,9 @@
 package org.rachel.midterm.publish
 
 import android.util.Log
-import androidx.databinding.InverseMethod
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.CollectionReference
-import kotlinx.coroutines.*
 import org.rachel.midterm.`object`.Article
 import org.rachel.midterm.`object`.Author
 import java.util.*
@@ -17,21 +13,31 @@ class PublishViewModel : ViewModel() {
     var title = MutableLiveData<String>()
     var tag = MutableLiveData<String>()
     var content = MutableLiveData<String>()
+    var isWayne = MutableLiveData<Boolean>()
+    private val author1 = Author(email = "wayne@school.com.tw", id = "waynechen323", name = "AKA小安老師")
+    private val author2 = Author(email = "rachel@dot.com.tw", id = "dot123", name = "I'm Rachel")
+    lateinit var author: Author
+
+    init {
+        isWayne.value = false
+    }
 
     fun publishData(articles: CollectionReference) {
         val document = articles.document()
         // Create a new user with a first and last name
-        val author1 = Author(email = "wayne@school.com.tw", id = "waynechen323", name = "AKA小安老師")
-        val author2 = Author(email = "rachel@dot.com.tw", id = "dot123", name = "I'm Rachel")
+        author = if (isWayne.value == true) {
+            author1
+        } else
+            author2
         val data = Article(
-            author1,
+            author,
             content = content.value ?: "HA HA HA",
             createdTime = Calendar.getInstance().timeInMillis,
             id = document.id,
             title = title.value ?: "Someone forget enter title",
             tag = tag.value ?: ""
         )
-        Log.i("HAHA","Data: $data")
+        Log.i("HAHA", "Wayne?: ${isWayne.value},  Data: $data")
         document.set(data)
     }
 
